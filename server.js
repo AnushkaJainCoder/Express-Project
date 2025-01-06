a011ec392ecd6b56c4be6aa00cee73dbb86bd19d
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-06 15:24:04 +0530

    create a server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-// const  chalk  = require('chalk');
-import chalk from 'chalk';
- console.log("hello");
- console.log(chalk.blue('go'));
+// // const  chalk  = require('chalk');
+// import chalk from 'chalk';
+//  console.log("hello");
+//  console.log(chalk.blue('go'), chalk.red('leave me'), chalk.yellow('now'));
  
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req,res)=>{
+    res.send('hello home page');
+});
+app.get('/whether', (req, res) => {
+    res.send('Ur whether');
+});
+app.listen(3000, () => {
+    console.log("server is running");
+    
+})
